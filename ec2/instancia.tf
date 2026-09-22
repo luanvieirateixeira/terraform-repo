accecc4a798b49c2a23d19447d7ee3922df0aabc
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-22 13:28:10 -0300

    fix: remove az preference

--- ec2/instancia.tf
+++ ec2/instancia.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "EC2-Terraform" {
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"
-  availability_zone = "us-east-1a"
   key_name = data.aws_key_pair.terraform-key.key_name
   subnet_id = data.terraform_remote_state.network.outputs.subnet_id_terraform
   security_groups = [data.terraform_remote_state.network.outputs.security_group_id_terraform]
