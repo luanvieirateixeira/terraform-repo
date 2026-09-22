0cefa77ad8dd9cdd2f57e2115e85a27af2c2b28f
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-22 10:48:01 -0300

    create a EC2 and outputs

---
+++ ec2/instancia.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "EC2-Terraform" {
+  ami           = "ami-0b6d9d3d33ba97d99"
+  instance_type = "t3.micro"
+  availability_zone = "us-east-1a"
+  key_name = data.aws_key_pair.terraform-key.key_name
+  security_groups = output.subnet_id
+  subnet_id = security_group_id_terraform
+
+  
+  root_block_device{
+    volume_size = 9
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "EC2-Terraform"
+  }
+}
+
+data "aws_key_pair" "terraform-key" {
+  key_name = "terraform-teste"
+}
