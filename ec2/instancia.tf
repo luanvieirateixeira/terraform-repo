015ab4bc166c237b4b05fcef4a9199320c71ccd8
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-22 16:58:40 +0000

    format with terraform fmt

--- ec2/instancia.tf
+++ ec2/instancia.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "EC2-Terraform" {
-  ami           = "ami-0b6d9d3d33ba97d99"
-  instance_type = "t3.micro"
-  key_name = data.aws_key_pair.terraform-key.key_name
-  subnet_id = data.terraform_remote_state.network.outputs.subnet_id_terraform
-  security_groups = [data.terraform_remote_state.network.outputs.security_group_id_terraform]
+  ami                         = "ami-0b6d9d3d33ba97d99"
+  instance_type               = "t3.micro"
+  key_name                    = data.aws_key_pair.terraform-key.key_name
+  subnet_id                   = data.terraform_remote_state.network.outputs.subnet_id_terraform
+  security_groups             = [data.terraform_remote_state.network.outputs.security_group_id_terraform]
   associate_public_ip_address = true
 
-  
-  root_block_device{
+
+  root_block_device {
     volume_size = 9
     volume_type = "gp3"
   }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,9 +1,9 @@
 output "subnet_id_terraform" {
-    description = "ID da Subnet"
-    value = aws_subnet.subnet_terraform.id
+  description = "ID da Subnet"
+  value       = aws_subnet.subnet_terraform.id
 }
 
 output "security_group_id_terraform" {
   description = "ID do Security Group"
-  value = aws_security_group.security_group_terraform.id
+  value       = aws_security_group.security_group_terraform.id
 }
