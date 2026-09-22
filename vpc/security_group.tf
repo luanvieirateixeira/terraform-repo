587866ed072e58ec6c133d26f8686621c781931e
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-22 10:35:41 -0300

    renome the sg

--- vpc/security_group.tf
+++ vpc/security_group.tf
@@ -1,15 +1,15 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
+resource "aws_security_group" "security_group_terraform" {
+  name        = "Security_group_terraform"
+  description = "Permite conexão SSH, ICMP e trafego por toda internet"
   vpc_id      = aws_vpc.vpc_terraform.id
 
   tags = {
-    Name = "allow_tls"
+    Name = "SG Terraform"
   }
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.security_group_terraform.id
   cidr_ipv4         = aws_vpc.vpc_terraform.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
@@ -17,7 +17,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.security_group_terraform.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
   ip_protocol       = "tcp"
@@ -25,13 +25,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.security_group_terraform.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_icmp" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.security_group_terraform.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = -1
   ip_protocol       = "icmp"
