85ad2632bc78015698cb0ba2d0c055a83cb43da0
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-22 10:20:37 -0300

    feat: allow ssh

--- vpc/security_group.tf
+++ vpc/security_group.tf
@@ -16,6 +16,14 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   to_port           = 443
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.vpc_terraform.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
