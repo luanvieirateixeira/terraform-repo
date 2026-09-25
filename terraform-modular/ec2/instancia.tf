1a8442a3a146d1a46938c42abf771c5d836e43d8
luanvieirateixeira <luan.trader98@gmail.com>
2026-09-25 15:41:19 -0300

    fix:  modules

--- terraform-modular/ec2/instancia.tf
+++ terraform-modular/ec2/instancia.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "EC2-Terraform" {
   ami                         = "ami-0b6d9d3d33ba97d99"
   instance_type               = "t3.micro"
   key_name                    = data.aws_key_pair.terraform-key.key_name
-  subnet_id                   = module.network.subnet_id_terraform
-  security_groups             = [module.network.security_group_id_terraform]
+  subnet_id                   = module.vpc.subnet_id_terraform
+  security_groups             = [module.vpc.security_group_id_terraform]
   associate_public_ip_address = true
   availability_zone = "us-east-1a"
 
